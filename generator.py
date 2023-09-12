5a3f63bc29bc2f59986849e97292b28dc8f62473
mikejmz24 <miguelbass2009@gmail.com>
2023-09-11 20:08:44 -0700

    Corrected logic for Customer list creation

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import uuid
 import csv
 import customer
 
-customer_list: list = []
+customer_list: list[customer.Customer] = []
 
 
 def create_UUID() -> uuid:
@@ -24,16 +24,28 @@ def read_csv_into_customer_object(file: str) -> list:
         for index, row in enumerate(reader):
             if index > 0:
                 result.append(customer.Customer(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13],
-                                    row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28]))
+                                                row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26], row[27], row[28]))
     return result
 
 
-def create_record_with_uuid(record: list[customer.Customer]) -> list:
+def create_record_with_uuid(record: list[customer.Customer]) -> list[customer.Customer]:
+    result: list[customer.Customer] = []
     for row in record:
         new_uuid: str = create_UUID()
         row.id = new_uuid
-    return list
+        result.append(row)
+    return result
+
+# TODO: Add logic to create n records per list record 
+def create_multiple_records(record: list[customer.Customer], multiple: int) -> list[customer.Customer]:
+    result: list[customer.Customer] = []
+    inner_result: list[customer.Customer] = []
+    for row in record:
+        inner_result = []
+        for x in range(0, multiple):
+            inner_result = create_record_with_uuid(customer_list[0:x])
+    return result
+
 
 customer_list = read_csv_into_customer_object('marketing_campaign.csv')
-customr_list = create_record_with_uuid(customer_list)
-print(customer_list[0:1])
+customer_list = create_record_with_uuid(customer_list)
--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,9 +1,14 @@
 import generator
 import pytest
+import customer
+import uuid
+
+customer_list: list[customer.Customer] = generator.read_csv_into_customer_object(
+    'marketing_campaign.csv')
 
 
 def test_create_UUID() -> None:
-    assert len(generator.create_UUID()) > 0
+    assert isinstance(generator.create_UUID(), uuid.UUID)
 
 
 def test_create_UUID_is_unique() -> None:
@@ -12,8 +17,33 @@ def test_create_UUID_is_unique() -> None:
 
 
 def test_read_csv_record_is_not_empty() -> list:
-    assert len(generator.read_csv_into_customer_object('marketing_campaign.csv')) > 0
+    assert len(generator.read_csv_into_customer_object(
+        'marketing_campaign.csv')) > 0
 
 
 def test_record_is_assigned_a_uuid() -> None:
-    pass
+    customer_list = generator.create_record_with_uuid(customer_list[0:1])
+    example_customer: customer.Customer = customer.Customer(customer_list[0].id, customer_list[0].year_birth, customer_list[0].education, customer_list[0].marital_status,
+                                                            customer_list[0].income, customer_list[0].kidhome, customer_list[
+                                                                0].teenhome, customer_list[0].dt_customer,
+                                                            customer_list[0].recency, customer_list[0].mnt_wines, customer_list[
+                                                                0].mnt_fruits, customer_list[0].mnt_meat_products,
+                                                            customer_list[0].mnt_fish_products, customer_list[
+                                                                0].mnt_sweet_products, customer_list[0].mnt_gold_prods,
+                                                            customer_list[0].num_deals_purchases, customer_list[
+                                                                0].num_web_purchases, customer_list[0].num_catalog_purchases,
+                                                            customer_list[0].num_store_purchases, customer_list[
+                                                                0].num_web_visits_month, customer_list[0].accepted_cmp3,
+                                                            customer_list[0].accepted_cmp4, customer_list[
+                                                                0].accepted_cmp5, customer_list[0].accepted_cmp1,
+                                                            customer_list[0].accepted_cmp2, customer_list[
+                                                                0].complain, customer_list[0].z_cost_contact,
+                                                            customer_list[0].z_revenue, customer_list[0].response)
+    assert isinstance(example_customer.id, uuid.UUID)
+
+
+def test_add_2_records_with_uuid() -> None:
+    n: int = 2
+    result: list[customer.Customer] = []
+    
+        
